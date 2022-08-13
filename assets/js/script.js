c366942a262a596f5d1bc1fd9db429ccda26518e
Enzo Zanatta de Sant'Ana <89364741+enzozsantana@users.noreply.github.com>
2022-08-13 15:33:35 -0300

    Adição da animação de Game Over

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,19 +13,22 @@ const jump = ({ keyCode }) => {
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
+    let marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
 
         mario.style.animation = 'none';
-        mario.style.bottom = `${marioPosition}px`;
-
         mario.src = 'assets/images/game-over.png';
         mario.style.width = '75px';
         mario.style.marginLeft = '50px';
-
+        
+        let downInterval = setInterval(() => {
+            marioPosition -= 20;
+            mario.style.bottom = `${marioPosition}px`;
+        }, 100);
+        
         clearInterval(loop);
     }
 }, 10);
